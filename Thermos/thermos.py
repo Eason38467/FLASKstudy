400191bb2211cd163fb5fed9f43ef66d50016e15
Easonxiao <630478562@qq.com>
2020-09-06 22:55:04 +0800

    flask helo world demo + html

--- Thermos/thermos.py
+++ Thermos/thermos.py
@@ -2,11 +2,23 @@ from flask import Flask, render_template,url_for
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def initials(self):
+        return "{}. {}".format(self.firstname[0],self.lastname[0])
+    def __stre__(self):
+        return
+
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     #return 'hello world'
-    return render_template('index.html')
+    return render_template('index.html', title = "this is title", text = "this is text", user= User('siyuan','xiao'))
 
 
 if __name__ == '__main__':
